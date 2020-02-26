7488df0f9786d65cfed47c28a156f10a55be2cd4
Ruben <rmontill@student.42madrid.com>
2020-02-26 18:46:20 +0100

    Algunos cambios realizados en el renexec.sh

--- ej02/renexec.sh
+++ ej02/renexec.sh
@@ -2,11 +2,22 @@
 
 NUM=$#
 
-if (test $NUM -eq 1)
+if (test $NUM -gt 2)
 then
-	SUF=exec
-else
-	SUF=$2
+	echo Más de dos parametros introducidos
+	exit 1
+fi
+if (test $NUM -lt 1)
+then 
+	echo Faltan parámetros en la llamada
+	exit 1
+else 
+	if (test $NUM -eq 2)
+	then
+		SUF=.$2
+	else
+		SUF=.exec
+	fi
 fi
 
 #Dos opciones:
@@ -15,3 +26,16 @@ fi
 
 #find -type f para ver todos los archivos, pero habria que hallar una manera de ver sus permisos
 
+for file in $(find $1 -type f)
+do
+	mod=$(echo $file | rev | cut -f1 -d '/' | rev)
+	echo mod = $mod
+
+	echo calvo
+	echo $file
+	echo $mod
+	if (test -x $mod)
+	then
+		echo siii #quitar la extension y meter la otra
+	fi
+done
