2cdcd208c63511f3d38cfb8b4a27a2f8b158e892
Ruben Montilla <r.montillaf.2019@alumnos.urjc.es>
2020-03-02 12:13:19 +0100

    Ahora si que si está acabado

--- ej02/renexec.sh
+++ ej02/renexec.sh
@@ -20,16 +20,9 @@ else
 	fi
 fi
 
-echo $SUF
-echo calvooo
-
 for file in $(find $1 -perm /111 -type f)
-do
-	echo file = $file	
+do	
 	mod=$(echo $file | rev | sed -E 's/[a-zA-Z]*\.//' | rev)
 	mod=$(echo $mod$SUF)
-	echo mod = $mod
 	mv $file $mod
 done
-
-echo calvoooo
